e063ccf54b5fcfcbdc0133fc05d153c570f28700
ONZ1 <onzi1230@gmail.com>
2021-05-11 17:28:00 +0900

    Create sort.h

---
+++ pointer4/pointer4/sort.h
@@ -0,0 +1,11 @@
+#ifndef __SORT_H__
+#define __SORT_H__
+
+#include <stdio.h>
+
+void SwapCallByReference(int* a, int* b);
+void sort(int* ptr);
+void out(int* ptr);
+void change(int array[], int* ptr);
+
+#endif // !__SORT_H__
